04ff3886256d1c7078d9ff5d1d46900b6074a862
zfeldstein <zackfeldstein@gmail.com>
2022-07-07 22:45:40 -0500

    initial commit

---
+++ web-service/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+)
+// album represents data about a record album.
+// type album struct {
+//     ID     string  `json:"id"`
+//     Title  string  `json:"title"`
+//     Artist string  `json:"artist"`
+//     Price  float64 `json:"price"`
+// }
+
+// getAlbums responds with the list of all albums as JSON.
+func getCloud(c *gin.Context) {
+	return_message := gin.H{
+		"response" : "CLOUD",
+	}
+    c.IndentedJSON(http.StatusOK, return_message)
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/cloud", getCloud)
+
+    router.Run("localhost:8080")
+}
